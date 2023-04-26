17df95e147a4ba3111587eff3dfbdc0c7a4ba125
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-04-26 02:25:46 +0100

    create re-useable spacer widget for height and width spacing

---
+++ lib/src/ui/home/widgets/constants/height_space_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HeightSpaceWidget extends StatelessWidget {
+  const HeightSpaceWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 15,
+    );
+  }
+}
