dac281bf8f3fe4fdf6f6b98e964b6706de369cdc
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-04-26 02:25:05 +0100

    create app color class

---
+++ lib/src/core/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color bgColor = Color(0xff00f0f0f);
+  static const Color lightBlack = Color(0xff01e1b1e);
+  static const Color purple = Color(0xff08326fd);
+  static const Color mustardYellow = Color(0xff0ffc13c);
+  static const Color cyan = Color(0xff000c399);
+  static const Color lightPink = Color(0xff0ff6d7a);
+  static const Color starkWhite = Color(0xff0fdfdfd);
+
+}
