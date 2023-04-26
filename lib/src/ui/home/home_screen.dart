7af8d6c3d583ff0798ae0842df60cd8b2d019e69
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-04-26 02:30:10 +0100

    add the home screen with different possible layout depending on the available width

---
+++ lib/src/ui/home/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_project/src/ui/home/widgets/desktop_view.dart';
+import 'package:portfolio_project/src/ui/home/widgets/mobile_view.dart';
+import 'package:portfolio_project/src/ui/home/widgets/tab_view.dart';
+
+import '../../core/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: AppColors.bgColor,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 450){
+            return const MobileView();
+          }
+          else if (constraints.maxWidth >= 1024) {
+            return const DesktopView();
+          }else {
+            return const TabView();
+
+          }
+
+        }
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
