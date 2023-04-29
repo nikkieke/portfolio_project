a8883f60a48169ae85361d9054b4c4f2f4ea6ac3
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-04-29 03:19:24 +0100

    update textview widget with autosize text package to help text be more responsive

--- lib/src/ui/home/widgets/constants/text_view.dart
+++ lib/src/ui/home/widgets/constants/text_view.dart
@@ -1,7 +1,9 @@
+
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../../../../core/colors.dart';
 
 class TextView extends StatelessWidget {
   TextAlign? textAlign;
@@ -12,25 +14,28 @@ class TextView extends StatelessWidget {
   double? letterSpacing;
   double? wordSpacing;
   int? maxLines;
+  Color? color;
+
 
 
 
   TextView({Key? key,this.fontWeight,  required this.text,this.overflow, this.textAlign,
-    this.wordSpacing,this.letterSpacing,this.maxLines,
+    this.wordSpacing,this.letterSpacing,this.maxLines,this.color,
     this.size= 0,  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
+    return AutoSizeText(
       text,
-      maxLines: maxLines,
+      minFontSize: 12,
+      maxLines: 9,
       textAlign: textAlign,
       style: GoogleFonts.openSans(
         textStyle: TextStyle(
-            color: AppColors.starkWhite,
+            color: color,
             fontWeight: fontWeight,
             fontSize:size==0? 20 :size,
-            overflow: overflow,
+            overflow: TextOverflow.ellipsis,
             letterSpacing: letterSpacing,
             wordSpacing: wordSpacing,
             decoration: TextDecoration.none
@@ -42,4 +47,5 @@ class TextView extends StatelessWidget {
     );
   }
 }
+
 
