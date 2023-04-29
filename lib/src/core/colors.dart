bf39c50159867e35a094802d25bb0f6b8be63f57
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-04-29 03:03:30 +0100

    add more colors

--- lib/src/core/colors.dart
+++ lib/src/core/colors.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class AppColors{
   static const Color bgColor = Color(0xff00f0f0f);
   static const Color lightBlack = Color(0xff01e1b1e);
+  static const Color lightWhite = Color(0xff0969696);
   static const Color purple = Color(0xff08326fd);
   static const Color mustardYellow = Color(0xff0ffc13c);
   static const Color cyan = Color(0xff000c399);
+  static const Color blue = Color(0xff02868b2);
+  static const Color iconBg = Color(0xff0262427);
   static const Color lightPink = Color(0xff0ff6d7a);
-  static const Color starkWhite = Color(0xff0fdfdfd);
+  static const Color starkWhite = Color(0xff0ffffff);
+  static const Color leadBlack =  Color(0xff0000000);
 
 }
