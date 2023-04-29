e8873368066f3059329d289744d8f4c458a87f01
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-04-29 03:21:22 +0100

    Build stats widget as reuseable widget

--- lib/src/ui/home/widgets/constants/stat_widget.dart
+++ lib/src/ui/home/widgets/constants/stat_widget.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_project/src/ui/home/widgets/constants/text_view.dart';
 class StatWidget extends StatelessWidget {
-  const StatWidget({
+   StatWidget({
     super.key,
-    required this.height,
-    required this.color
+       this.height,
+      required this.color,
+      this.textview,
+      this.textviewTitle,
   });
 
-  final double height;
+   double? height;
   final Color color;
+  TextView? textview;
+   TextView? textviewTitle;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.center,
       height: height,
       width: double.maxFinite,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(20),
       ),
-      child:TextView(text: '',),);
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: textviewTitle,
+          ),
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: textview,
+          ),
+        ],
+      )
+    );
   }
 }
